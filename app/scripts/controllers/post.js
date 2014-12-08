f96376888f14959e4e2050f52617071fcc295f37
Pedrum Golriz <pedrum.golriz@agilex.com>
2014-12-07 21:43:53 -0500

    HAUS-3. Parse Facebook Events complete

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*global $:false*/
 /**
  * @ngdoc function
  * @name showhausAngApp.controller:PostCtrl
@@ -7,6 +7,7 @@
  * # PostCtrl
  * Controller of the showhausAngApp
  */
+var genreArray = ['list', 'grid', 'limited'];
 function getCookie(cname) {
 	var name = cname + '=';
 	var ca = document.cookie.split(';');
@@ -18,6 +19,41 @@ function getCookie(cname) {
 	}
 	return '';
 }
+var FB;
+window.fbAsyncInit = function () {
+	FB.init({
+		appId: '204851123020578',
+		status: true,
+		xfbml: true
+	});
+};
+(function (d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {
+		return;
+	}
+	js = d.createElement(s);
+	js.id = id;
+	js.src = 'https://connect.facebook.net/en_US/all.js';
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+/* jshint ignore:start */
+$(window).bind("blur focus focusin focusout load resize scroll unload click " +
+	"dblclick mousedown mouseup mousemove mouseover mouseout mouseenter " +
+	"mouseleave change select submit keydown keypress keyup error", function () {
+	$('select').chosen().trigger('liszt:updated');
+	$('select').on('change', function () {
+		$('#postshow_venue').trigger('chosen:updated');
+	});
+	if (!CKEDITOR.instances['editor'])
+		CKEDITOR.replace('editor');
+});
+$('a[name="rulesagreement"]').on('click', function () {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 'slow');
+});
+/* jshint ignore:end */
 var venues = [];
 var preUrl = 'http://v2.showhaus.org/';//set to blank for release
 angular.module('showhaus')
@@ -74,7 +110,8 @@ angular.module('showhaus')
 				'price': $scope.price,
 				'description': $scope.description,
 				'tags': document.getElementById('tags').value,
-				'email': $scope.email
+				'email': $scope.email,
+				'fbimage': $scope.fbImage
 			};
 			$http.post(
 				preUrl+'assets/new.php',
@@ -85,4 +122,125 @@ angular.module('showhaus')
 				console.log(status);
 			});
 		};
+		$scope.fbEvent = function(){
+			var facebookEvent = $scope.facebookEvent.toLowerCase();
+			facebookEvent = facebookEvent.split('events/').pop().split('/')[0];
+			var eventNum = parseInt(facebookEvent.split('events/').pop().split('/')[0]);
+			if(!isNaN(eventNum)){
+				FB.api(
+					{
+						method: 'fql.query',
+						query: 'SELECT pic_cover FROM event WHERE eid = ' + facebookEvent
+					},
+					function (response) {
+						if (response && !response.error) {
+							$scope.$apply(function() {
+								var fbImage = response[0].pic_cover.source; //jshint ignore:line
+								$scope.fbImage = fbImage;
+							});
+						}
+						else{
+							console.log(response.error);
+						}
+					}
+				);
+				FB.api(
+					{
+						method: 'fql.query',
+						query: 'SELECT email FROM user WHERE uid=me()'
+					},
+					function (response){
+						$scope.$apply(function() {
+							var fbEmail = response[0].email;
+							$scope.email = fbEmail;
+						});
+					}
+				);
+				FB.api(
+					facebookEvent,
+					function(response){
+						console.log(response);
+						$scope.$apply(function(){
+							$scope.title = response.name;
+							$scope.subtitle = $scope.title.substring(80, 190);
+							/* jshint ignore:start */
+							CKEDITOR.instances.editor.setData('',function(){
+								this.insertText(response.description);
+							});
+							/* jshint ignore:end */
+							if(response.venue.state.toLowerCase() === 'dc' || response.venue.state.toLowerCase() === 'd.c' || response.venue.state.toLowerCase() === 'washington dc'){
+								$scope.citySelect = 'DC';
+							}
+							else{
+								$scope.citySelect = response.venue.city;
+							}
+							for(var q = 0; q < $scope.venues.length; q++){
+								if(response.location === $scope.venues[q][1]){
+									$scope.venue = $scope.venues[q][1];
+								}
+							}
+							if(!$scope.venue){
+								$scope.venue = 'newvenue';
+								$scope.newvenuename = response.location;
+								$scope.newvenueaddress = response.venue.street;
+							}
+
+
+							var dateTime = response.start_time; //jshint ignore:line
+							dateTime = dateTime.split('T');
+							var fbDate = dateTime[0];
+							fbDate = fbDate.split('-');
+							fbDate = fbDate[1] + '/' + fbDate[2] + '/' + fbDate[0];
+							var fbTime = fbDate[1];
+							var timeHour = dateTime[1].substr(0, 2);
+							var timeMins = dateTime[1].substr(2, 3);
+							var ampm = 'AM';
+							if (timeHour > 12) {
+								timeHour -= 12;
+								ampm = 'PM';
+							}
+							fbTime = timeHour + timeMins + ' ' + ampm;
+							$scope.date = fbDate;
+							$scope.time = fbTime;
+							var tag1;
+							for (var i = 0; i < genreArray.length; i++) {
+								var genre = $scope.description.indexOf(genreArray[i]);
+								if (genre !== -1) {
+									tag1 = genreArray[i];
+								}
+							}
+							$('#tags').select2('val',[tag1]);
+							var price = $scope.description.split('$');
+							price = price[1];
+
+							if (price) {
+								var twodigi = isNaN(price.substr(0, 2));
+								var onedigi = isNaN(price.substr(0, 1));
+								if (twodigi === false) {
+									price = price.substr(0, 2);
+								}
+								else if (onedigi === false) {
+									price = price.substr(0, 1);
+								}
+							}
+							else {
+								price = 0;
+							}
+							$scope.price = price;
+						});
+						$('.filter_city').trigger('chosen:updated');
+						$('.filter_city').trigger('chosen:updated');
+					}
+				);
+			}
+			else{
+				console.log('facebook event parse failed');
+			}
+		};
+		$scope.fbLogin = function(){
+			FB.login(function () {}, {
+				scope: ''
+			});
+		};
+		$scope.genreArray = ['grid','list'];
 	});
