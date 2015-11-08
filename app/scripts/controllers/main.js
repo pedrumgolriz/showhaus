e635fea31b09f6a457e96b11e0d807b661ade2fc
g007 <g007@g007s-MBP.home>
2015-11-08 17:32:14 -0500

    fixing main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -193,4 +193,32 @@ angular.module('showhaus')
 			href: 'https://maps.google.com/maps?q='+address+'%2C'+city+'&ie=UTF8&t=m&output=embed'
 		});
 	};
+
+	$scope.getDay = function(date){
+	    var day;
+    	switch (new Date(date).getDay()) {
+            case 0:
+                day = "Sunday";
+                break;
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+        }
+        return day;
+	}
   });
