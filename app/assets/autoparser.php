15c4f114edbfd524780c76a4b554a5f4d9c59879
g007 <g007@g007s-MBP.home>
2015-12-04 02:21:58 -0500

    taking out a log and an exit

--- app/assets/autoparser.php
+++ app/assets/autoparser.php
@@ -89,7 +89,6 @@
         } catch(Facebook\Exceptions\FacebookResponseException $e) {
             // When Graph returns an error
             echo 'Graph returned an error: ' . $e->getMessage();
-            exit;
         } catch(Facebook\Exceptions\FacebookSDKException $e) {
             // When validation fails or other local issues
             echo 'Facebook SDK returned an error: ' . $e->getMessage();
